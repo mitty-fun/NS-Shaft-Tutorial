a67d5d3f6b848b179aea297ca9197781b8eebf06
pika <owpika@gmail.com>
2016-12-04 12:39:46 +0800

    add baseURL

--- mission_5/index.js
+++ mission_5/index.js
@@ -10,15 +10,17 @@ var ceiling;
 
 function preload () {
 
-    game.load.spritesheet('player', '../assets/player.png', 32, 32);
-    game.load.image('wall', '../assets/wall.png');
-    game.load.image('ceiling', '../assets/ceiling.png');
-    game.load.image('normal', '../assets/normal.png');
-    game.load.image('nails', '../assets/nails.png');
-    game.load.spritesheet('conveyorRight', '../assets/conveyor_right.png', 96, 16);
-    game.load.spritesheet('conveyorLeft', '../assets/conveyor_left.png', 96, 16);
-    game.load.spritesheet('trampoline', '../assets/trampoline.png', 96, 22);
-    game.load.spritesheet('fake', '../assets/fake.png', 96, 36);
+    game.load.baseURL = 'https://wacamoto.github.io/NS-Shaft-Tutorial/assets/';
+    game.load.crossOrigin = 'anonymous';
+    game.load.spritesheet('player', 'player.png', 32, 32);
+    game.load.image('wall', 'wall.png');
+    game.load.image('ceiling', 'ceiling.png');
+    game.load.image('normal', 'normal.png');
+    game.load.image('nails', 'nails.png');
+    game.load.spritesheet('conveyorRight', 'conveyor_right.png', 96, 16);
+    game.load.spritesheet('conveyorLeft', 'conveyor_left.png', 96, 16);
+    game.load.spritesheet('trampoline', 'trampoline.png', 96, 22);
+    game.load.spritesheet('fake', 'fake.png', 96, 36);
 }
 
 function create () {
